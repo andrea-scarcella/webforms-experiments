46e51242ad719a619b322fc59ba666b04c93ce4e
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2014-03-03 00:16:41 +0100

    Completed wingtipToys tutorial part 5

--- WingtipToys/WingtipToys/ProductList.aspx.cs
+++ WingtipToys/WingtipToys/ProductList.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -13,5 +15,16 @@ namespace WingtipToys
         {
 
         }
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
+        }
+
     }
 }
